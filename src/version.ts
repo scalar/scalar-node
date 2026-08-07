87a1102a13ba9d1a1469e84ddf1af5814d7b40ce
Marc Laventure <marc@scalar.com>
2026-08-07 16:13:27 -0700

    Update version from 0.1.0 to 0.3.1

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,3 @@
 // File generated from our OpenAPI spec by Scalar. See README.md for details.
 
-<<<<<<< HEAD
 export const VERSION = "0.3.1"; // x-release-please-version
-=======
-export const VERSION = '0.1.0'; // x-release-please-version
->>>>>>> 5b4ec77ba62174c569f6146c00cab597d4177e49
